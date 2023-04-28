0e3d3c48fb7becb8520fb1265fbe6542ef57f938
mayssa riahi <mayssa.riahi@esprit.tn>
2023-04-28 06:07:09 +0100

    token and user stored

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,20 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { TokenStorageService } from '../service/token-storage.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit {
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  roles: string[] = [];
 
   username: string ="";
   email: string ="";
   password: string ="";
 
-  constructor(private router: Router,private http: HttpClient  ) { }
+  constructor(private router: Router,private http: HttpClient,private storageService: TokenStorageService  ) { }
+
   showSignUp() {
     document.getElementById('container')!.classList.add("right-panel-active");
   }
@@ -23,6 +28,12 @@ export class LoginComponent {
     document.getElementById('container')!.classList.remove("right-panel-active");
   }
 
+  ngOnInit(): void {
+    if (this.storageService.isLoggedIn()) {
+      this.isLoggedIn = true;
+      this.roles = this.storageService.getUser().roles;
+    }
+  }
 
   loginUser(){
     console.log(this.username);
@@ -34,6 +45,13 @@ export class LoginComponent {
     };
 
     this.http.post("http://localhost:8075/api/auth/signin", bodyData).subscribe((resultData: any)=>{
+      this.storageService.saveToken(resultData.accessToken);
+      this.storageService.saveUser(resultData);
+      this.isLoginFailed = false;
+      this.isLoggedIn = true;
+      this.roles = this.storageService.getUser().roles;
+      console.log("login user");
+
       console.log(resultData);
 
      
@@ -42,6 +60,10 @@ export class LoginComponent {
 
     });
  
+  }
+  
+  reloadPage(): void {
+    window.location.reload();
   }
 
 
