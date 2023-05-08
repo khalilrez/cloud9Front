b5b609e9e3c9bcc42bc614e1e637d44c59abd2e4
khalil rezgui <khalilrezgui0@gmail.com>
2023-05-08 09:15:55 +0100

    More fixes doctor Appointment component + login

--- src/app/doctor-appointment/doctor-appointment.component.ts
+++ src/app/doctor-appointment/doctor-appointment.component.ts
@@ -1,7 +1,7 @@
 import { User } from './../home/user';
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../home/user.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AppointmentService } from '../appointment/appointment.service';
 import { Appointment } from '../appointment/appointment';
 
@@ -13,12 +13,19 @@ import { Appointment } from '../appointment/appointment';
 export class DoctorAppointmentComponent implements OnInit{
 
   appointments: Appointment[] = [];
-  constructor(private appointmentService: AppointmentService){}
+  constructor(private appointmentService: AppointmentService,private router:Router){}
 
   ngOnInit(): void {
     this.appointmentService.getAppointments().subscribe(appointments => {
       this.appointments = appointments;
       console.log(this.appointments);
     });
+  }
+
+  goToConsultationFileAsDoctor(id:number,user:number) {
+    console.log("going to /consultation-file-edit")
+    console.log(id)
+    // Navigate to the consultation file component with the consultationFile object
+    this.router.navigate(['/consultation-file-edit'], { state: { appointmentId: id ,patientId:user} });
   }
 }
